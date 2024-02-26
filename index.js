26e475e5eb01b8ea8471eda8f682322c0c125009
Pavle Paunovic <paavlee@yahoo.com>
2024-02-26 12:11:15 +0100

    app

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const cors = require('cors');
+const PORT = 8080;
+
+app.use(cors())
+
+app.get('/api/top-headlines-serbia', async (req, res) => {
+  try {
+    const url = 'https://newsapi.org/v2/top-headlines?' +
+      'country=rs&' +
+      'apiKey=65f5bf5de3ce404c853c0aac623504d6';
+
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
