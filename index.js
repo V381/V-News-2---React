693d46be852cbe124aef6347ee6cddac7e862505
Pavle <V381@users.noreply.github.com>
2024-02-26 12:27:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 
