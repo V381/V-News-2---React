ff14f5b5e167da9352f18bdb450353a4fe74fb96
Pavle <V381@users.noreply.github.com>
2024-02-26 13:05:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Content-Type", "application/json");
+  next();
+});
 
 app.get('/api/top-headlines-serbia', async (req, res) => {
   try {
